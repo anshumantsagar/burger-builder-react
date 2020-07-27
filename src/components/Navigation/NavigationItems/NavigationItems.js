00728dbfce0163f55d2d4190388fd0b5b12f7853
anshumantsagar <anshumantsagar@gmail.com>
2020-07-27 20:27:36 +0530

    add logout route

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,11 +6,13 @@ import classes from './NavigationItems.css';
 //components
 import NavigationItem from '../NavigationItems/NavigationItem/NavigationItem'
 
-const navigationItems = () => (
+const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
         <NavigationItem link="/orders" >Orders</NavigationItem>
-        <NavigationItem link="/auth">Authenticate</NavigationItem>
+        {!props.isAuthenticated 
+            ? <NavigationItem link="/auth">Authenticate</NavigationItem> 
+            : <NavigationItem link="/logout">Logout</NavigationItem>}
     </ul>
 );
 
